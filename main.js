c50c1e008e796c0d0bdade6ce7d0cc3039e8326f
dphefadu <dphefadu@student.wethinkcode.co.za>
2021-05-24 09:30:17 +0200

    First commit

---
+++ main.js
@@ -0,0 +1,12 @@
+function logon(){
+    const username = document.getElementById("username").value;
+    const psw = document.getElementsById("psw").value;
+    alert('hello'+username);
+    document.getElementsById('username').style.background ="red";
+    // if (username.length <6){
+    //     alert('not valid');
+    // }
+    // if(psw.length <0){
+    //     alert('not valid');
+    // }
+}
